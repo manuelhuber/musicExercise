500928d576d49c3d55b05f6d40d140eeb251e686
Manuel Huber <nelo112@web.de>
2016-09-29 21:06:25 +0200

    Added more texts

--- codebase/app/data/translate/de.ts
+++ codebase/app/data/translate/de.ts
@@ -1,10 +1,13 @@
 export const de : any = {
+  'selectDifficulty': 'Schwierigkeit auswählen:',
+  'seconds': 'Sekunden',
   'fingerControl': {
     'title': 'Finger Kontrolle',
     'heading': 'Haben Sie ihre Finger unter Kontrolle?',
     'explanation': 'Bewegen sie nur die unten aufgeführten Finger. Die Buchstaben L und R stehen für die linke bzw ' +
     'rechte Hand. Die Zahl dahinter steht für den Finger. 1 = Daumen, 5 = kleiner Finger. Klicken sie auf den großen' +
-    'Button um die nächsten Finger zu trainieren.'
+    'Button um die nächsten Finger zu trainieren.',
+    'automaticNext': 'Automatisch weiterschalten alle'
   },
   'intervals': {
     'title': 'Intervalle',
