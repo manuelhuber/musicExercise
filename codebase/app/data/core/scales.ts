fe97f4cbf9c51ad4c3c2c6c588211cbe6de80c40
Manuel Huber <nelo112@web.de>
2016-10-04 20:41:12 +0200

    Added scales

---
+++ codebase/app/data/core/scales.ts
@@ -0,0 +1,29 @@
+export const MAJOR : number[] = [2, 2, 1, 2, 2, 2, 1];
+export const MINOR : number[] = [2, 1, 2, 2, 1, 2, 2];
+export const HARMONIC_MINOR : number[] = [2, 1, 2, 2, 1, 3, 1];
+export const HUNGARIAN_MINOR : number[] = [2, 1, 3, 1, 1, 3, 1];
+export const HUNGARIAN_MAJOR : number[] = [3, 1, 2, 1, 2, 1, 2];
+export const BLUES : number[] = [3, 2, 1, 1, 3, 2];
+export const PENTATONIC_MAJOR : number[] = [2, 2, 3, 2, 3];
+export const PENTATONIC_MINOR : number[] = [3, 2, 2, 3, 2];
+export const SPANISH_EIGHT : number[] = [1, 2, 1, 1, 1, 2, 2, 2];
+export const ORIENTAL : number[] = [1, 3, 1, 1, 3, 1, 2];
+export const REDUCED : number[] = [1, 2, 1, 2, 1, 2, 1, 2];
+export const OVERTONE : number[] = [2, 2, 2, 1, 2, 1, 2];
+export const ENIGMATIC : number[] = [1, 3, 2, 2, 2, 1, 1];
+
+export const SCALES : number[][] = [
+  MAJOR,
+  MINOR,
+  HARMONIC_MINOR,
+  HUNGARIAN_MINOR,
+  HUNGARIAN_MAJOR,
+  BLUES,
+  PENTATONIC_MAJOR,
+  PENTATONIC_MINOR,
+  SPANISH_EIGHT,
+  ORIENTAL,
+  REDUCED,
+  OVERTONE,
+  ENIGMATIC
+];
